77b9cd3d5b82df59802db5e20c6eef0894c9945c
Luca Bulletti <luca.bulletti@students.unibe.ch>
2019-03-14 16:51:59 +0100

    cleanup because of Elio.

--- medlandscape-app/src/App.js
+++ medlandscape-app/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello world</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ medlandscape-app/src/components/Map.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default Map;
