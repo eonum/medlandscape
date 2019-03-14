5bcb78637fa9d89ac7cc5be36187ad0ad03614b6
Elio <elio.f-8@hotmail.com>
2019-03-14 17:37:03 +0100

    map component starting

--- medlandscape-app/src/App.js
+++ medlandscape-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Map from './components/Map.js';
 import './App.css';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>Hello world</h1>
+                <Map />
             </div>
         );
     }
