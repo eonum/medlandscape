c449f3154b475bef0f121f2dc7457aebd1c0111e
Luca Bulletti <luca.bulletti@students.unibe.ch>
2019-03-14 17:39:02 +0100

    first api call

--- medlandscape-app/src/App.js
+++ medlandscape-app/src/App.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+    state = {
+        apiURL :  "https://qm1.ch/",
+        apiRequest : "de/api/medical_landscape/hospitals"
+    }
+
+    apiCall = () => {
+        fetch(this.state.apiURL + this.state.apiRequest).then(res => res.json()).then((result) => {
+            console.log(result);
+        });
+    };
+
+
     render() {
+        this.apiCall();
         return (
             <div className="App">
                 <h1>Hello world</h1>
