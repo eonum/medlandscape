d5beadac04b4ffbf188aaa0dd0034ad96355d35c
Robin Bürkli <robin.buerkli@students.unibe.ch>
2019-03-15 14:05:38 +0100

    Implemented first draft of DropdownMenu

---
+++ medlandscape-app/src/components/DropdownMenu/DropdownMenu.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './DropdownMenu.css';
+
+class DropdownMenu extends Component {
+
+  state = {
+    selectedName: "Kennzahl",
+    dropdownContentClasses: "dropdown-content hide"
+  }
+
+  componentDidMount() {
+    // this.setState({ selectedName: this.props.listItems[0].name_de });
+  }
+
+  toggleDropdown() {
+    var css = (this.state.dropdownContentClasses === "dropdown-content hide") ? "dropdown-content show" : "dropdown-content hide";
+    this.setState({ dropdownContentClasses:css });
+  }
+
+  selectItem(item) {
+    this.setState({ selectedName: item.name_de });
+    this.toggleDropdown();
+  }
+
+  filterFunction() {
+    let input, filter, a, i, div, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("div");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="dropdown">
+      <button onClick={this.toggleDropdown.bind(this)} className="dropbtn">{this.state.selectedName} ▼</button>
+      <div id="myDropdown" className={this.state.dropdownContentClasses}>
+      <input type="text" placeholder="Suchen..." id="myInput" onKeyUp={this.filterFunction.bind(this)} />
+      {
+        this.props.listItems.map((item) => (
+            //task.done === false && <TodoItem key={task.id} task={task} toggle={this.props.toggle}/>
+            // prop.toCheck === compareValue && <ComponentToLoadIfTrue />
+            <div onClick={this.selectItem.bind(this, item)}>{item.name_de}</div>
+        ))
+      }
+      </div>
+      </div>
+
+      //this.props.listItems.map((item) => (
+
+      //))
+    );
+  }
+}
+
+
+
+// PropTypes
+DropdownMenu.propTypes = {
+  listItems: PropTypes.array.isRequired, // Yes, the comma is necessary
+}
+
+export default DropdownMenu;
