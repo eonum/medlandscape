f5c0cf0e94695959e67a5d359c2045337959a7fa
Roland Widmer <roland.widmer@students.unibe.ch>
2019-03-18 22:12:52 +0100

    first version with working react-leaflet component

--- medlandscape-app/src/App.js
+++ medlandscape-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Map from './components/Map.js';
+import MapComponent from './components/MapComponent.js';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +7,7 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>Hello world</h1>
-                <Map />
+                <MapComponent />
             </div>
         );
     }
---
+++ medlandscape-app/src/components/MapComponent.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import './MapComponent.css';
+
+class MapComponent extends Component {
+  state = {
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13,
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng]
+    return (
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+    )
+  }
+}
+
+export default MapComponent;
