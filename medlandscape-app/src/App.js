6a56e7d6ddb9f1096acd58e79757def63d6f98f5
Luca Bulletti <luca.bulletti@students.unibe.ch>
2019-03-19 10:33:02 +0100

    cleanup after conflicts

--- medlandscape-app/src/App.js
+++ medlandscape-app/src/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import DropdrownMenu from './components/DropdownMenu.js';
 import CantonList from './components/CantonList.js';
-=======
 import CantonMap from './components/CantonMap/CantonMap.js';
->>>>>>> 3e16a9cdb998c4a84ece03abc9e63e56fb53847a
 import './App.css';
 
 const apiURL = "https://qm1.ch/";
@@ -73,14 +70,11 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-<<<<<<< HEAD
                 <DropdrownMenu listItems={this.state.var} selectItem={this.dropdownSelectItem}
                     selectedItem={this.state.selectedVariable} />
                 <CantonList cantons={this.state.cantons} selectCanton={this.selectCanton} selectedCantons={this.selectedCanton}/>
-=======
                 <h1>Hello world</h1>
                 <CantonMap/>
->>>>>>> 3e16a9cdb998c4a84ece03abc9e63e56fb53847a
             </div>
         );
     }
