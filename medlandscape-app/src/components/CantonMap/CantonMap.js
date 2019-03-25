dca2146e7aeec13883d25cec121f0b97f471827a
Roland Widmer <roland.widmer@students.unibe.ch>
2019-03-25 21:49:52 +0100

    - created a single JSON-file containing all cantons

--- medlandscape-app/src/components/CantonMap/CantonMap.js
+++ medlandscape-app/src/components/CantonMap/CantonMap.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Map, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet'
 import './CantonMap.css';
+import cantons from './cantons/cantons.json';
 import NW from './cantons/NW.json';
 import OW from './cantons/OW.json';
 
@@ -18,21 +19,42 @@ class CantonMap extends Component {
 		console.log(this.props.cantons);
 	}
 
-  render() {
-    const position = [this.state.lat, this.state.lng]
-    const nw = NW;
-    const ow = OW;
-    return (
-      <Map center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
-        />
-        <GeoJSON data = {nw}/>
-        <GeoJSON data = {ow}/>
-      </Map>
-    )
-  }
+	/*returns data value*/
+	returnData(item, keys){
+		let temp = item.attributes;
+		for (let i = 0; i < keys.length; i++){
+			if(temp == null) {
+				return 0;
+			} else {
+				temp = temp[keys[i]];
+			}
+		}
+		if (temp === undefined)
+			temp = 0;
+		return temp;
+	}
+	
+	
+	render() {
+		const position = [this.state.lat, this.state.lng]
+		return (
+			<Map center={position} zoom={this.state.zoom}>
+				<TileLayer
+				  attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+				  url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+				/>
+				{
+					this.props.data.data.map((item) => (
+						<GeoJSON data = {cantons[item.name]}>
+							<Popup>
+								{this.returnData(item, this.props.data.keys)}
+							</Popup>
+						</GeoJSON>
+					))
+				}
+			</Map>
+		)
+	}
 }
 
 export default CantonMap;
