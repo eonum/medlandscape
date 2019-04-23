1071fd4bd8d505729a3ca321787c09e331d3797b
Roland Widmer <roland.widmer@students.unibe.ch>
2019-04-23 15:07:40 +0200

    FilterEditor first example

--- medlandscape-app/src/components/FilterEditor/FilterEditor.js
+++ medlandscape-app/src/components/FilterEditor/FilterEditor.js
@@ -4,11 +4,25 @@ import React, { Component } from 'react';
  * Example component to show how to work with react-i18next localization
  */
 class FilterEditor extends Component {
-
+	constructor(props){
+		super(props);
+		
+	}
+		
+	filter() {
+		var i;
+		var array = [];
+		for(i = 0; i < 10; i++){
+			array.push(this.props.hospitals[i]);
+		}
+		updateSelectedHospitals(array);
+	}
+	
     render () {
-
         return (
-            "test"     
+            <button onClick={filter}>
+				First 10 Hospitals
+			</button>     
         );
     }
 }
