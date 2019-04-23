3c682a5f76010e48c3962fb14fe4e93f9efcee1a
Roland Widmer <roland.widmer@students.unibe.ch>
2019-04-23 16:02:02 +0200

    Dropdown for enum variable added to the FilterEditor component

--- medlandscape-app/src/components/FilterEditor/FilterEditor.js
+++ medlandscape-app/src/components/FilterEditor/FilterEditor.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
+import DropdownMenu from '../DropdownMenu/DropdownMenu.js';
 
 /**
  * Example component to show how to work with react-i18next localization
  */
 class FilterEditor extends Component {
+	state = {
+		variables: []
+	};
+	
+	componentDidUpdate() {
+		if(this.props.hasLoaded && this.state.variables.length == 0){
+			
+			let enumVariables = this.findEnumVariables()
+			this.setState({variables: enumVariables});
+		}
+	}
 
+	findEnumVariables = () => {
+		let i;
+		let enumVariables = [];
+		
+		for(i = 0; i < this.props.variables.length; i++){
+			if(this.props.variables[i].variable_type == "enum") {
+				enumVariables.push(this.props.variables[i]);
+			}
+		}
+		return enumVariables;
+	}
+	
+	/**
+    * 
+    */
+    dropdownSelectItem = (item) => {
+        console.log(item);
+    }
+	
+	
+	
 	filter = () => {
 		if (this.props.hasLoaded){
 		var i;
@@ -18,9 +51,12 @@ class FilterEditor extends Component {
 
     render () {
         return (
-            <button onClick={this.filter}>
-				First 10 Hospitals
-			</button>
+			<div className="control-panel">
+				<button onClick={this.filter}>
+					First 10 Hospitals
+				</button>
+				<DropdownMenu id="filterDropDown" listItems={this.state.variables} selectItem={this.dropdownSelectItem} selectedItem={this.state.variables[0]} />
+			</div>
         );
     }
 }
