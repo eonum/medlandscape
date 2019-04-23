c90a16a7054474bf7d72cd56bca2ea553aa4f538
Roland Widmer <roland.widmer@students.unibe.ch>
2019-04-23 16:19:19 +0200

    CheckboxList modification

--- medlandscape-app/src/components/CheckboxList/CheckboxListItem.js
+++ medlandscape-app/src/components/CheckboxList/CheckboxListItem.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 class CheckboxListItem extends Component {
 
     render() {
-        const {name, text, checkboxSelectItem} = this.props.object;
+       // const {name, text, checkboxSelectItem} = this.props.object;
         return (
             <div className="checkbox-list-item">
                 <label>
                     <input type="checkbox" className="list-item-cb" onChange={this.props.checkboxSelectItem.bind(this, this.props.object)}/>
                     <div className="list-item-name">
-                        {(text) ? name + " " + text : name}
+                        {this.props.object}
                     </div>
                 </label>
             </div>
