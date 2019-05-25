7ab9df4d57a89a41fa065169501ad86be0049238
Elio <elio.f-8@hotmail.com>
2019-05-25 23:05:40 +0200

    correlation added

--- medlandscape-app/src/utils.mjs
+++ medlandscape-app/src/utils.mjs
@@ -11,7 +11,6 @@
 export function numberFormat(number){
     let nmbr = Math.round(number*100)/100;
     let str = nmbr.toString();
-    console.log(str);
     let index;
     index = str.indexOf(".");
     if (index = str.indexOf(".") === -1){
@@ -22,4 +21,42 @@ export function numberFormat(number){
         str = str.substring(0, index) + "'" + str.substring(index, str.length);
     }
     return str;
+}
+
+/**
+* calculate pearson correlation between two given data arrays
+* @param {Array} x first data array
+* @param {Array} y second data array
+* @return {number} r the pearson correlation
+**/
+export function pearsonCorrelation(x, y){
+    let n = x.length;
+    let sum_X = 0, sum_Y = 0, sum_XY = 0;
+    let squareSum_X = 0, squareSum_Y = 0;
+
+    for (let i = 0; i < n; i++)
+    {
+        // sum of elements of array X.
+        sum_X = sum_X + x[i];
+
+        // sum of elements of array Y.
+        sum_Y = sum_Y + y[i];
+
+        // sum of X[i] * Y[i].
+        sum_XY = sum_XY + x[i] * y[i];
+
+        // sum of square of array elements.
+        squareSum_X = squareSum_X + x[i] * x[i];
+        squareSum_Y = squareSum_Y + y[i] * y[i];
+    }
+
+    // use formula for calculating correlation coefficient r.
+    let r = (n * sum_XY - sum_X * sum_Y)/
+                (Math.sqrt((n * squareSum_X -
+                sum_X * sum_X) * (n * squareSum_Y -
+                sum_Y * sum_Y)));
+
+    r = Math.round(r*1000)/1000;
+
+    return r;
 }
